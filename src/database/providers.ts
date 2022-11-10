17b620266b0c8b155d4f4eea737e4d7d55811705
Sangkun-svg <gims4752@gmail.com>
2022-11-10 23:55:33 +0900

    build : connection

--- src/database/providers.ts
+++ src/database/providers.ts
@@ -8,8 +8,8 @@ export const databaseProviders = [
         type: 'mysql',
         host: 'localhost',
         port: 3306,
-        username: 'root',
-        password: 'root',
+        username: 'root', // replaece dotenv constant
+        password: '1234', // replaece dotenv constant
         database: 'test',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, //WARN: Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
