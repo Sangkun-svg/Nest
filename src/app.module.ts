6002249df739f198782a0886f622421f31064a8f
Sangkun-svg <gims4752@gmail.com>
2022-11-11 00:28:45 +0900

    feat/ generate item curd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ItemModule } from './item/item/item.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
