a3b67499e60ee6507799beeb5b777dff2a9abef3
Sangkun-svg <gims4752@gmail.com>
2022-11-10 23:54:51 +0900

    feat : generate simple crud api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
+import { DatabaseModule } from 'src/database/module';
+
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-
+import { userProviders } from './providers';
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [...userProviders, UserService],
 })
 export class UserModule {}
