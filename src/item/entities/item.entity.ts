1ed4d0fa18f7ebdf5f500798048950d0d1e1df31
Sangkun-svg <gims4752@gmail.com>
2022-11-11 00:52:43 +0900

    feat : add coulmns

--- src/item/entities/item.entity.ts
+++ src/item/entities/item.entity.ts
@@ -1,1 +1,19 @@
-export class Item {}
+import { User } from 'src/user/entities/user.entity';
+import { Column, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Item {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @OneToMany((type) => User, (user) => user) // ??
+  user: User;
+
+  @Column()
+  userId: number;
+}
