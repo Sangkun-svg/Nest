23d06050906fb54cdcf85106881c91db5d536f08
Sangkun-svg <gims4752@gmail.com>
2022-11-11 00:23:04 +0900

    feat : add User column

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,9 +2,15 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
   @Column({ length: 500 })
   name: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column()
+  createAt: Date;
 }
