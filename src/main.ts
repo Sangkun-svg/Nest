2462c2c3b0121be5143e3a83770bb1b83618c78c
Sangkun-svg <gims4752@gmail.com>
2022-11-12 22:45:57 +0900

    feat : add logger condition

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   setupSwagger(app);
   app.setGlobalPrefix('v1');
   await app.listen(9000);
